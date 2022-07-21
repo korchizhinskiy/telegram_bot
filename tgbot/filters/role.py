3715e6ffe3dcb1030066d4c9ecacef9970a71a49
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-21 16:32:38 +0700

    Add *args, **kwargs for convenient use.

--- tgbot/filters/role.py
+++ tgbot/filters/role.py
@@ -1,5 +1,4 @@
 from typing import Union
-from aiogram.types import Message
 
 from aiogram.dispatcher.filters import BaseFilter
 
@@ -9,9 +8,8 @@ class Role_Filter(BaseFilter):
     """Filter for role of users."""
     user_role: Union[object, list]
 
-
-    async def __call__(self, message, role) -> bool:
+    async def __call__(self, *args, **data) -> bool:
         if isinstance(self.user_role, object):
-            return role == self.user_role
+            return data["role"] == self.user_role
         else:
-            return role in self.user_role
+            return data["role"] in self.user_role
