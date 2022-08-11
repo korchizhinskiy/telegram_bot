74473de185091ac0a126a81ce4d3df4b1f93ecee
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-11 19:51:41 +0300

    Add databasemiddleware and connect to router

---
+++ tgbot/middlewares/database.py
@@ -0,0 +1,40 @@
+from typing import Any, Awaitable, Callable
+
+from aiogram.dispatcher.middlewares.base import BaseMiddleware
+from aiogram.dispatcher.flags.getter import get_flag
+from aiogram.types import Message
+from asyncpg import Pool
+
+from tgbot.services.repository import Repo
+
+
+class DatabaseMiddleware(BaseMiddleware):
+
+    def __init__(self, pool: Pool):
+        super().__init__()
+        self.pool = pool
+
+
+    async def __call__(
+            self,
+            handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: dict[str, Any]
+            ) -> Any:
+        
+        async with self.pool.acquire() as connection: # AttributeError TODO
+            async with connection.transaction():
+
+                data["database"] = connection
+                database_type = get_flag(data, "database_type")
+                if database_type:
+                    match database_type:
+                        case "repo":
+                            data["repo"] = Repo(connection)
+                else:
+                    data["repo"] = None
+                        
+                result = await handler(event, data)
+
+                del data["repo"]
+                return result
--- tgbot/routes/__init__.py
+++ tgbot/routes/__init__.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher, Router
+from tgbot.middlewares.database import DatabaseMiddleware
 
 from tgbot.routes.admin.admin_menu import admin_menu_router
 from tgbot.routes.user.welcome import user_welcome_router
@@ -6,11 +7,14 @@ from tgbot.middlewares.role import AdminCheckerMiddleware
 
 
 
-def register_all_routers(dp: Dispatcher, config, pool) -> None:
+def register_all_routers(dp: Dispatcher, config, connection) -> None:
     """Register routers into main router."""
     master_router = Router()
     # Setup MiddleWares.
     master_router.message.outer_middleware(AdminCheckerMiddleware(config.tg_bot.admin_id))
+    master_router.callback_query.outer_middleware(AdminCheckerMiddleware(config.tg_bot.admin_id))
+    master_router.message.middleware(DatabaseMiddleware(connection))
+    master_router.callback_query.middleware(DatabaseMiddleware(connection))
     
     # Add local routers.
     master_router.include_router(admin_menu_router)
